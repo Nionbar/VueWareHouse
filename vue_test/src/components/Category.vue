af5d9ea4ff7275edb23f9d958dced3dba2853130
Nionbar <2388803303@qq.com>
2022-04-08 22:08:58 +0800

    Vue默认插槽

---
+++ vue_test/src/components/Category.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="categroy">
+      <h3>{{title}}</h3>
+        <slot>我是默认值，当用户没有指定时我会出现！</slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'Category',
+        props:['title','listData']
+    }
+</script>
+
+<style>
+    .categroy {
+        background-color: skyblue;
+        width: 200px;
+        height: 300px;
+    }
+    h3{
+        text-align: center;
+        background-color:orange;
+    }
+</style>
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 引入插件
-import vueResources from 'vue-resource'
+
 //关闭生成提示
 Vue.config.productionTip = false
 
-Vue.use(vueResources)
 new Vue({
     el:'#app',
-    beforeCreate(){
-        Vue.prototype.$bus = this
-    },
     render:h=>h(App)
 })
