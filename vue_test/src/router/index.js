b2274a178856fb2c351be9626d1c3382a4e9a698
Nionbar <2388803303@qq.com>
2022-04-10 19:15:35 +0800

    路由的基本使用
    多级路由

---
+++ vue_test/src/router/index.js
@@ -0,0 +1,28 @@
+
+import VueRouter from 'vue-router'
+import About from '../pages/About'
+import Home from '../pages/Home'
+import Messages from '../pages/Message'
+import News from '../pages/News'
+export default new VueRouter({
+    routes:[
+        {
+            path:'/about',
+            component:About
+        },
+        {
+            path:'/home',
+            component:Home,
+            children: [
+                {
+                    path:'message',
+                    component:Messages
+                },
+                {
+                    path:'news',
+                    component:News
+                }
+            ]
+        }
+    ],
+})
